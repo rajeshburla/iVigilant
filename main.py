b00a58d76c80b53877e350d89b30b89366bcc243
krishzinx <krishzinx@gmail.com>
2019-03-06 22:12:14 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import pandas as pd
 import numpy as np
 from xml.dom import minidom
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
+import re
+
+
+def replaceURL(df):
+    df1 = pd.DataFrame(columns = df.columns)
+    for index, row in df.iterrows():
+        text = row['text']
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+        for url in urls:
+            text = text.replace(url, '<URLURL>')
+        df1.loc[index] = [text, row['HorBot'], row['gender']]
+    return df1
+
+def replaceUserMentions(df):
+    df1 = pd.DataFrame(columns = df.columns)
+    for index, row in df.iterrows():
+        text = row['text']
+        mentions = re.findall('(^|[^@\w])@(\w{1,15})', text)
+        #print(mentions)
+        for mention in mentions:
+            text = text.replace('RT @' + mention[1] + ':',  '<UserMention>')
+            text = text.replace('@' + mention[1], '<UserMention>')
+        df1.loc[index] = [text, row['HorBot'], row['gender']]
+    return df1
 
 columns  = ['text', 'HorBot', 'gender']
 df = pd.DataFrame(columns = columns)
@@ -21,4 +45,8 @@ with open('en/truth-train.txt') as f:
         df.loc[i] = arr
         i = i + 1
         #alltext = alltext.append(alltext1)
-df.to_csv('truth-train', sep='\t')
+df.to_csv('truth-train1', sep='\t')
+df = replaceURL(df)
+df = replaceUserMentions(df)
+df.to_csv('truth-train-new1', sep='\t')
+print(df)
